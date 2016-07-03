2c7cffc6fd3ef7edad194eafef2d85c853c8a93d
Ricco <riccochapa@gmail.com>
2016-07-03 01:00:27 -0500

    Update

--- fifth_to_last.js
+++ fifth_to_last.js
@@ -1,3 +1,28 @@
-var DataStructures = require('algorithms').DataStructures;
-var Stack = DataStructures.Stack;
-var Queue = DataStructures.Queue;
+function Deque(){
+ this.stac = [];
+ this.popback = function(){
+  return this.stac.pop();
+ }
+ this.pushback = function(item){
+  this.stac.push(item);
+ }
+ this.popfront = function(){
+  return this.stac.shift();
+ }
+ this.pushfront = function(item){
+  this.stac.unshift(item);
+ }
+};
+
+function test(one, two, three) {
+  var checkThree = new Deque();
+  var random = Math.floor(Math.random() * three * 13);
+  for(var i = 0; i < random; i++){
+    checkThree.pushfront(Math.floor(Math.random() * two * random));
+    checkThree.pushback(Math.floor(Math.random() * one * random));
+  }
+  console.log("Test Array: " + checkThree.stac);
+  checkThree.popback(),checkThree.popback(),checkThree.popback(),checkThree.popback(); console.log("Fifth from last: " + checkThree.popback());
+}
+
+test(33, 222, 69); //You can enter any number but larger numbers will take time to process.
