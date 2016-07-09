56df8c73fd8833636e43122b92931003db6ce02e
Ricco <riccochapa@gmail.com>
2016-07-08 21:47:26 -0500

    converted to airbnb style guide

--- fifth_to_last.js
+++ fifth_to_last.js
@@ -1,28 +1,29 @@
-function Deque(){
- this.stac = [];
- this.popback = function(){
-  return this.stac.pop();
- }
- this.pushback = function(item){
-  this.stac.push(item);
- }
- this.popfront = function(){
-  return this.stac.shift();
- }
- this.pushfront = function(item){
-  this.stac.unshift(item);
- }
-};
+function Deque() {
+  this.stac = [];
+  this.popback = function popback() {
+    return this.stac.pop();
+  };
+  this.pushback = function pushback(item) {
+    this.stac.push(item);
+  };
+  this.popfront = function popfront() {
+    return this.stac.shift();
+  };
+  this.pushfront = function pushfront(item) {
+    this.stac.unshift(item);
+  };
+}
 
 function test(one, two, three) {
-  var checkThree = new Deque();
-  var random = Math.floor(Math.random() * three * 13);
-  for(var i = 0; i < random; i++){
+  const checkThree = new Deque();
+  const random = Math.floor(Math.random() * three * 13);
+  for (let i = 0; i < random; i++) {
     checkThree.pushfront(Math.floor(Math.random() * two * random));
     checkThree.pushback(Math.floor(Math.random() * one * random));
   }
-  console.log("Test Array: " + checkThree.stac);
-  checkThree.popback(),checkThree.popback(),checkThree.popback(),checkThree.popback(); console.log("Fifth from last: " + checkThree.popback());
+  checkThree.popback(); checkThree.popback();
+  checkThree.popback(); checkThree.popback();
+  checkThree.popback();
 }
 
-test(33, 222, 69); //You can enter any number but larger numbers will take time to process.
+test(33, 222, 69);
